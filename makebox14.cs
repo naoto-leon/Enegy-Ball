a01e9636b771afdb0ba4710a9cf3b8b90b2a29ee
naoto-leon <naoto.leon@gmail.com>
2019-09-02 22:25:35 +0900

    Initial commit

---
+++ makebox14.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makebox14 : MonoBehaviour
+{
+
+  public  GameObject Prefabsbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var paprent = this.transform;
+
+        GameObject boxx = (GameObject)Instantiate(Prefabsbox, transform.position, Random.rotation);
+        Destroy(boxx, 8f);
+    }
+}
